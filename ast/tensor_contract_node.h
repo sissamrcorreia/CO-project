5658a4d2f47430aa7ff86b54d1b30eab88cac5b1
Francisca Almeida <francisca.vicente.de.almeida@tecnico.ulisboa.pt>
2025-05-16 16:17:45 +0100

    refactor: change tensor_contract_node to inherit from binary_operation_node

--- ast/tensor_contract_node.h
+++ ast/tensor_contract_node.h
@@ -7,21 +7,14 @@ namespace udf {
    * Implements tensor contractions (e.g., `t1 ** t2` for matrix
    * multiplication-like operations).
    */
-  class tensor_contract_node : public cdk::expression_node {
-    cdk::expression_node *_left;
-    cdk::expression_node *_right;
-
+  class tensor_contract_node : public cdk::binary_operation_node {
   public:
     tensor_contract_node(int lineno, cdk::expression_node *left,
                         cdk::expression_node *right,
                         const std::string &operation)
-        : cdk::expression_node(lineno), _left(left), _right(right) {}
-
-    cdk::expression_node *left()  { return _left;  }
-    cdk::expression_node *right() { return _right; }
+      : cdk::binary_operation_node(lineno, left, right) {}
 
     void accept(basic_ast_visitor *sp, int level) { sp->do_tensor_contract_node(this, level); }
-
   };
 
 } // udf
