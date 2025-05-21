f57ace556154348a05d2a3562943b7d00d5a0ba8
Cecília Correia <cecilia.correia@tecnico.ulisboa.pt>
2025-05-21 17:37:59 +0100

    refactor: nodes: for, reshape

--- ast/for_node.h
+++ ast/for_node.h
@@ -8,22 +8,23 @@ namespace udf {
   /**
    * Class for describing for-cycle nodes.
    */
-  class for_node : public cdk::basic_node {
-    cdk::expression_node *_init;
-    cdk::expression_node *_condition;
-    cdk::expression_node *_step;
-    cdk::basic_node *_instruction;
+  class for_node: public cdk::basic_node {
+    cdk::sequence_node *_declaration;
+    cdk::sequence_node *_condition;
+    cdk::sequence_node *_increment;
+    cdk::basic_node *_block;
 
   public:
-    for_node(int lineno, cdk::expression_node *init, cdk::expression_node *condition, cdk::expression_node *step,
-             cdk::basic_node *instruction) :
-        cdk::basic_node(lineno), _init(init), _condition(condition), _step(step), _instruction(instruction) {
-    }
+    for_node(int lineno, cdk::sequence_node *declaration,
+                       cdk::sequence_node *condition, cdk::sequence_node *increment,
+                       cdk::basic_node *block)
+            : cdk::basic_node(lineno), _declaration(declaration),
+              _condition(condition), _increment(increment), _block(block) {}
 
-    cdk::expression_node* init()      { return _init;        }
-    cdk::expression_node* condition() { return _condition;   }
-    cdk::expression_node* step()      { return _step;        }
-    cdk::basic_node* instruction()    { return _instruction; }
+    cdk::sequence_node *declaration() { return _declaration; }
+    cdk::sequence_node *condition()   { return _condition;   }
+    cdk::sequence_node *increment()   { return _increment;   }
+    cdk::basic_node *block()          { return _block;       }
 
     void accept(basic_ast_visitor *sp, int level) { sp->do_for_node(this, level); }
 
--- ast/tensor_reshape_node.h
+++ ast/tensor_reshape_node.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cdk/ast/expression_node.h>
+#include <cdk/ast/sequence_node.h>
+
 namespace udf {
   /**
    * Class for describing tensor reshaping nodes.
@@ -7,16 +10,16 @@ namespace udf {
    * (e.g., `t.reshape(3,2)`).
    */
   class tensor_reshape_node : public cdk::expression_node {
-    cdk::expression_node *_argument;
-    cdk::expression_node *_shape;
+    cdk::expression_node *_tensor;
+    cdk::sequence_node *_arguments;
 
   public:
-    tensor_reshape_node(int lineno, cdk::expression_node *argument,
-                        cdk::expression_node *shape)
-        : cdk::expression_node(lineno), _argument(argument), _shape(shape) {}
+    tensor_reshape_node(int lineno, cdk::expression_node *tensor, cdk::sequence_node *arguments)
+      : cdk::expression_node(lineno), _tensor(tensor), _arguments(arguments) {}
 
-    cdk::expression_node *argument() { return _argument; }
-    cdk::expression_node *shape()    { return _shape;    }
+    cdk::expression_node *tensor() { return _tensor; }
+    cdk::sequence_node *arguments() { return _arguments; }
+    cdk::expression_node *argument(size_t ix) { return dynamic_cast<cdk::expression_node*>(_arguments->node(ix)); }
 
     void accept(basic_ast_visitor *sp, int level) { sp->do_tensor_reshape_node(this, level); }
 
